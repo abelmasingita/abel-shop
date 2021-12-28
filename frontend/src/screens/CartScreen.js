4dfcf994211c5744b01a65519626def32c06d093
Abel Hlongwani <abel.hlongwani@integrove.com>
2021-12-28 21:43:21 +0200

    cart reducer done

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Link, useParams, useNavigate, useLocation } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import Message from '../components/Message'
+import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
+import { addToCart } from '../actions/cartActions'
 
 const CartScreen = () => {
-    return (
-        <div>
-            <h1>cart</h1>
-        </div>
-    )
+  //Hooks
+  let { id } = useParams()
+  const location = useLocation()
+
+  const productId = id
+  const qty = location.search ? Number(location.search.split('=')[1]) : 1
+
+  const dispatch = useDispatch()
+  const cart = useSelector((state) => state.cart)
+  const { cartItems } = cart
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(id, qty))
+    }
+  }, [dispatch, productId, qty])
+
+  return (
+    <div>
+      <h1>cart</h1>
+    </div>
+  )
 }
 
 export default CartScreen
