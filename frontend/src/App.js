e9bfbffb8efd93321f89ccc4ad4f67596301c25e
Abel Hlongwani <abel.hlongwani@integrove.com>
2021-12-28 12:24:01 +0200

    React router v6 done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,9 @@ import {Container} from 'react-bootstrap'
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 import Footer from './components/Footer';
 import Header from './components/Header';
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
-
+import CartScreen from './screens/CartScreen'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 function App() {
   return (
@@ -14,14 +14,18 @@ function App() {
       <main className='py-3'>
         <Container>
           <Routes>
-            <Route path='/' element={<HomeScreen/>} exact />
-            <Route path='/product/:id' element={<ProductScreen/>} />
+            <Route path='/' element={<HomeScreen />} exact />
+            <Route path='/product/:id' element={<ProductScreen />} />
+            <Route path='/cart'>
+              <Route path='' element={<CartScreen />} />
+              <Route path=':id' element={<CartScreen />} />
+            </Route>
           </Routes>
         </Container>
       </main>
       <Footer />
     </Router>
-  );
+  )
 }
 
 export default App;
